dd4ccade7986da3787bb6ff274bb6155c737f496
Cicero <cicerog.silvestre@gmail.com>
2026-03-05 21:02:03 -0300

    feat: implementa tela de login do GopherNet
    - Cria layout HTML da página de login
    - Adiciona estilos CSS para formulário e botões
    - Configura rotas / e /login
    - Implementa utilitário para carregamento de templates
    - Adiciona estrutura inicial do router com Gorilla Mux

---
+++ webapp/src/router/routes/routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Route representa todas as rotas da aplicacao web
+type Route struct {
+	URI               string
+	Method            string
+	Function          func(w http.ResponseWriter, r *http.Request)
+	reqAuthentication bool
+}
+
+// RouterConfig coloca todas as rodas dentro do router
+func RouterConfig(r *mux.Router) *mux.Router {
+	routes := loginRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
+
+	return r
+}
